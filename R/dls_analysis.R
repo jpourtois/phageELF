7eacc37981dd326754ba2fd61a3d9c9a0bf6ba29
Julie Pourtois <jp22@alumni.princeton.edu>
2023-03-03 14:30:07 -0800

    Improve figures, metric parameter and documentation

--- R/dls_analysis.R
+++ R/dls_analysis.R
@@ -5,19 +5,19 @@
 #' @param data A dataframe with DLS data. One row per sample
 #' @param lytic A dataframe with the titer associated with each treatment.
 #' @param training_pairs A dataframe with two columns, named 'treatment' and 'control'.
-#' @param metric A string specifying the DLS metric used. Must be a substring of column names in 'data', excluding the 'sample' column.
 #' @param testing_pairs Optional. A dataframe with two columns, named 'treatment' and 'control'.
+#' @param metric A character string specifying the DLS metric to use. Default is 'Intensity'. Other metrics typically include 'Volume' and 'Number'. Column names in 'data' must contain the substrings 'intens','volume' and 'number' respectively (excluding the 'sample' column).
+#' @param intercept If TRUE, include a non-zero intercept in the linear model. Default is TRUE.
 #' @export
 #' @examples
-#' run_all(dls_data, lytic_data, training_pairs, 'intens', testing_pairs)
+#' run_all(dls_data, lytic_data, training_pairs, testing pairs)
 
-run_all <- function(data, lytic, training_pairs, metric, testing_pairs, intercept = FALSE) {
+run_all <- function(data, lytic, training_pairs, testing_pairs, metric = "Intensity", intercept = TRUE) {
   
+  # Create table to store AUC difference
   diff_total <- tibble::tibble(treatment = character(), control = character(), AUC = numeric())
   
-  # Calculate AUC diff for each treatment pair
-  diff_total <- tibble::tibble(treatment = character(), control = character(), AUC = numeric())
-  
+  # Call AUC_diff() function to calculate AUC diff for each treatment pair
   for (i in unique(training_pairs$control)){
     
     treatments_in <- training_pairs$treatment[training_pairs$control == i]
@@ -29,7 +29,7 @@ run_all <- function(data, lytic, training_pairs, metric, testing_pairs, intercep
   }
   
   # Train the model
-  lm_total <- lm_train(diff_total, lytic)
+  lm_total <- lm_train(diff_total, lytic, intercept)
   
   training_data <- lm_total$model
   training_data$treatment <- diff_total$treatment
@@ -65,16 +65,20 @@ run_all <- function(data, lytic, training_pairs, metric, testing_pairs, intercep
 #' @param data A dataframe with DLS data. One row per sample
 #' @param treatments A vector of strings corresponding to a subset of the samples in 'data'.
 #' @param control A string corresponding to one sample in 'data'.
-#' @param metric A string specifying the DLS metric used. Must be a substring of column names in 'data', excluding the 'sample' column.
+#' @param metric A character string specifying the DLS metric to use. Default is 'Intensity'. Other metrics typically include 'Volume' and 'Number'. Column names in 'data' must contain the substrings 'intens','volume' and 'number' respectively (excluding the 'sample' column).
 #' @export
 #' @examples
-#' AUC_diff(dls_data, c('treatment_1', 'treatment_2'), 'control', 'intensities')
+#' AUC_diff(dls_data, c('treatment_1', 'treatment_2'), 'control', 'Intensity')
 
-AUC_diff <- function(data, treatments_in, control_in, metric) {
+AUC_diff <- function(data, treatments_in, control_in, metric = "Intensity") {
   
   names(data)[names(data) == 'Sample.Name'] <- 'sample'
+  
+  if (metric == 'Intensity') {
+    match.metric <- 'intens'
+  } else {match.metric <- metric}
  
-  dls <- data[,grep(metric,colnames(data), ignore.case = TRUE)]
+  dls <- data[,grep(match.metric,colnames(data), ignore.case = TRUE)]
   
   diff <- tibble::tibble(treatment = treatments_in, control = rep(control_in, length(treatments_in)), AUC = NaN)
   
@@ -105,18 +109,22 @@ AUC_diff <- function(data, treatments_in, control_in, metric) {
 #' @param data A dataframe with DLS data. One row per sample
 #' @param treatments A vector of strings corresponding to a subset of the samples in 'data'.
 #' @param control A string corresponding to one sample in 'data'.
-#' @param metric A string specifying the DLS metric used. Must be a substring of column names in 'data', excluding the 'sample' column.
+#' @param metric A character string specifying the DLS metric to use. Default is 'Intensity'. Other metrics typically include 'Volume' and 'Number'. Column names in 'data' must contain the substrings 'intens','volume' and 'number' respectively (excluding the 'sample' column).
 #' @param size Optional. A vector of the particle sizes associated with the DLS measurements in 'data'.
 #' @export
 #' @examples
-#' plot_DLS(dls_data, c('treatment_1', 'treatment_2'), 'control', 'intensities', size_vector)
+#' plot_DLS(dls_data, c('treatment_1', 'treatment_2'), 'control', 'Intensity', size_vector)
 
-plot_DLS <- function(data, treatments, control, metric, size){
+plot_DLS <- function(data, treatments, control, metric = 'Intensity', size){
   
     names(data)[names(data) == 'Sample.Name'] <- 'sample'
     
+    if (metric == 'Intensity') {
+      match.metric <- "intens"
+    } else {match.metric <- metric}
+    
     # Remove extra columns and add back columns with sample names
-    dls <- data[,grep(metric,colnames(data), ignore.case = TRUE)]
+    dls <- data[,grep(match.metric,colnames(data), ignore.case = TRUE)]
     
     if (!missing(size)){
       dls <- dls[,1:length(size)]
@@ -128,23 +136,26 @@ plot_DLS <- function(data, treatments, control, metric, size){
     dls <- dls[(dls$sample %in% treatments) | (dls$sample %in% control),]
     
     # Change to long format
-    dls_long <- tidyr::gather(dls, key = "size", value = "value", grep(metric,colnames(dls), ignore.case = TRUE))
+    dls_long <- tidyr::gather(dls, key = "size", value = "value", grep(match.metric,colnames(dls), ignore.case = TRUE))
     
     # Order by sample
     dls_long <- dls_long[order(dls_long$sample),]
     
     # Add size if provided or linear vector if not
     if (missing(size)) {
-      dls_long$xscale <- rep(10^(1:length(grep(metric,colnames(data), ignore.case = TRUE))/ncol(dls)), nrow(dls))
+      dls_long$xscale <- rep(10^(1:length(grep(match.metric,colnames(data), ignore.case = TRUE))/ncol(dls)), nrow(dls))
     }
     else { dls_long$xscale <- rep(size, nrow(dls))}
 
     # Plot one curve for each sample
     ggplot2::ggplot(dls_long, ggplot2::aes(x = xscale, y = value, color = sample)) + 
       ggplot2::geom_line() +
-      ggplot2::labs(x = 'Size', y = metric, color = 'Treatment') +
+      ggplot2::labs(x = bquote("D"[h]~"(nm)"), y = paste(metric, '(%)'), color = 'Treatment') +
       ggplot2::scale_x_continuous(trans='log10') +
-      ggplot2::theme_bw()
+      ggplot2::theme_classic() + 
+      ggplot2::theme(axis.text = ggplot2::element_text(size = 13), axis.title = ggplot2::element_text(size = 14), 
+                     legend.text = ggplot2::element_text(size = 14), legend.title = ggplot2::element_blank()) +
+      ggplot2::annotation_logticks(sides = 'b')
   
 }
 
@@ -153,12 +164,12 @@ plot_DLS <- function(data, treatments, control, metric, size){
 #' This function allows you to train a linear model of the titer loss associated with the difference in AUC calculated from DLS data. 
 #' @param AUC A dataframe with the difference in AUC for each treatment. 
 #' @param titer A dataframe with the titer associated with each treatment.
-#' @param intercept If TRUE, include a non-zero intercept in the linear model. Default is FALSE.
+#' @param intercept If TRUE, include a non-zero intercept in the linear model. Default is TRUE.
 #' @export
 #' @examples
 #' lm_train(AUC_data, titer_data)
 
-lm_train <- function(AUC, titer, intercept = FALSE) {
+lm_train <- function(AUC, titer, intercept = TRUE) {
   
   AUC$titer_loss <- NA
   
@@ -185,13 +196,12 @@ lm_train <- function(AUC, titer, intercept = FALSE) {
 #'
 #' This function allows you to plot the training data and the trained linear model of the titer loss 
 #' associated with the difference in AUC calculated from DLS data. 
-#' @param model Trained model. Output of lm_train.
-#' @param intercept If TRUE, include a non-zero intercept in the linear model. Default is FALSE.
+#' @param model A trained linear model. Output of lm_train.
 #' @export
 #' @examples
 #' plot_model(AUC_data, titer_data, 'control')
 
-plot_model <- function(model, intercept = FALSE) {
+plot_model <- function(model) {
 
   AUC_titer <- model$model
   
@@ -206,16 +216,18 @@ plot_model <- function(model, intercept = FALSE) {
     ggplot2::geom_point(data = AUC_titer, ggplot2::aes(x = AUC, y = titer_loss)) +
     ggplot2::geom_line(data = newdata, ggplot2::aes(x = AUC, y = pred)) +
     ggplot2::geom_ribbon(data = newdata, ggplot2::aes(x = AUC,ymin = lower, ymax = upper), alpha=0.3) +
-    ggplot2::labs(x = 'AUC Difference', y = 'Difference in lytic activity') +
-    ggplot2::theme_bw()
+    ggplot2::labs(x = 'AUC Difference (%)', y = 'Difference in lytic activity (log10)') +
+    ggplot2::theme_classic() + 
+    ggplot2::theme(axis.text = ggplot2::element_text(size = 13), axis.title = ggplot2::element_text(size = 14))
+  
   
 }
 
 #' Use linear model to predict titer loss based on a difference in AUC 
 #'
 #' This function allows you to predict titer loss based on a trained linear model and on a difference in AUC, which can be obtained with the function 'AUC_Diff'.  
-#' @param AUC A dataframe with the difference in AUC for each treatment. 
-#' @param model A linear model. 
+#' @param AUC A dataframe with the difference in AUC for each treatment. Output of AUC_diff().
+#' @param model A trained linear model. Output of lm_train().
 #' @export
 #' @examples
 #' lm_test(lm_model, AUC_data)
@@ -231,8 +243,8 @@ lm_test <- function(model, AUC) {
 #' Plot titer loss predictions
 #'
 #' This function plots titer loss predictions with the training dat and model  
-#' @param AUC A dataframe with the difference in AUC for each treatment. 
-#' @param model A linear model. 
+#' @param AUC A dataframe with the difference in AUC for each treatment. Output of AUC_diff().
+#' @param model A trained linear model. Output of lm_train().
 #' @export
 #' @examples
 #' plot_test_data(lm_model, AUC_data)
@@ -251,12 +263,14 @@ plot_test_data <- function(model, AUC) {
   newdata$lower <- predict(model, newdata, interval = 'confidence')[,'lwr']  
   
   ggplot2::ggplot()  +
-    ggplot2::geom_point(data = AUC_titer, ggplot2::aes(x = AUC, y = titer_loss)) +
+    ggplot2::geom_point(data = AUC_titer, ggplot2::aes(x = AUC, y = titer_loss, color = 'Training data')) +
     ggplot2::geom_line(data = newdata, ggplot2::aes(x = AUC, y = pred)) +
     ggplot2::geom_ribbon(data = newdata, ggplot2::aes(x = AUC,ymin = lower, ymax = upper), alpha=0.3) +
-    ggplot2::geom_point(data = predicted_titer, ggplot2::aes(x = AUC, y = titer_loss), color = 'red') +
-    ggplot2::labs(x = 'AUC Difference', y = 'Difference in lytic activity (log10)') +
-    ggplot2::theme_bw()
+    ggplot2::geom_point(data = predicted_titer, ggplot2::aes(x = AUC, y = titer_loss, color = 'Predictions')) +
+    ggplot2::labs(x = 'AUC Difference (%)', y = 'Difference in lytic activity (log10)') +
+    ggplot2::theme_classic() + 
+    ggplot2::theme(axis.text = ggplot2::element_text(size = 13), axis.title = ggplot2::element_text(size = 14), 
+                   legend.text = ggplot2::element_text(size = 14), legend.title = ggplot2::element_blank())
   
 }
 
